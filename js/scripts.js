f9985de9f6a0592ea49328daa10146068ba10773
stefxmas <143958588+stefxmas@users.noreply.github.com>
2023-11-05 16:16:27 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
-let pokemonList=[{name:"Charmander", type:"Fire", height:"0.6"},{name:"Machop", type:"fighting", height:"0.8"},{ name:"Gengar", type:["ghost","poison"], height:"1.5"}]
-console.log(pokemonList);
-
+let pokemonList=[
+    {name:"Charmander", type:"Fire", height:"0.6"},
+    {name:"Machop", type:"fighting", height:"0.8"},
+    { name:"Gengar", type:["ghost","poison"], height:"1.5"}
+    ];
+    
+    let pokemonAux = {name:"test", type:"Fire", height:"0.6"};
+    
+    pokemonList.push(pokemonAux);
+    console.log(pokemonList);
