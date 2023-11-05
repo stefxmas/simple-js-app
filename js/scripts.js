649a73986df08ff23f8d8d0a1a6fde1ae22ceda3
stefxmas <143958588+stefxmas@users.noreply.github.com>
2023-11-05 17:22:17 -0500

    pokemon list
    adding pokemon list into HTML

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,22 @@
 let pokemonList=[
     {name:"Charmander", type:"Fire", height:"0.6"},
-    {name:"Machop", type:"fighting", height:"0.8"},
-    { name:"Gengar", type:["ghost","poison"], height:"1.5"}
+    {name:"Machop", type:"fighting", height:"1.2"},
+    { name:"Gengar", type:["ghost","poison"], height:"3"}
     ];
     
-    let pokemonAux = {name:"test", type:"Fire", height:"0.6"};
+    for (let i=0; i<pokemonList.length; i++){
+      if (pokemonList[i].height <1 && pokemonList[i].height >0.5){
+        console.log(pokemonList[i].name + " is a small pokemon");
+        document.write('<p>' + pokemonList[i].name +  pokemonList[i].height + ' is a small pokemon</p>')
+      }else if (pokemonList[i].height >1 && pokemonList[i].height <1.5){
+        console.log(pokemonList[i].name + " is a medium pokemon");
+        document.write('<p>' + pokemonList[i].name +  pokemonList[i].height + ' is a medium pokemon</p>')
+      }else {
+        console.log(pokemonList[i].name + " is a large pokemon");
+        document.write('<p>' + pokemonList[i].name +  pokemonList[i].height +' is a large pokemon</p>')
+      }
+    }
     
-    pokemonList.push(pokemonAux);
+    pokemonList.push(pokemonList);
     console.log(pokemonList);
+    
