51ae4cc594e91ace00ddc787ed8bd65115b8a4e0
stefxmas <143958588+stefxmas@users.noreply.github.com>
2023-11-07 10:53:41 -0500

    pokemonList.push
    removed pokemonList.push
    make a space between the name and height

--- js/scripts.js
+++ js/scripts.js
@@ -7,16 +7,14 @@ let pokemonList=[
     for (let i=0; i<pokemonList.length; i++){
       if (pokemonList[i].height <1 && pokemonList[i].height >0.5){
         console.log(pokemonList[i].name + " is a small pokemon");
-        document.write('<p>' + pokemonList[i].name +  pokemonList[i].height + ' is a small pokemon</p>')
+        document.write('<p>' + pokemonList[i].name +' '+ pokemonList[i].height + ' '+ ' is a large pokemon</p>')
       }else if (pokemonList[i].height >1 && pokemonList[i].height <1.5){
         console.log(pokemonList[i].name + " is a medium pokemon");
-        document.write('<p>' + pokemonList[i].name +  pokemonList[i].height + ' is a medium pokemon</p>')
+        document.write('<p>' + pokemonList[i].name +' '+  pokemonList[i].height + ' is a medium pokemon</p>')
       }else {
         console.log(pokemonList[i].name + " is a large pokemon");
-        document.write('<p>' + pokemonList[i].name +  pokemonList[i].height +' is a large pokemon</p>')
+        document.write('<p>' + pokemonList[i].name +' '+  pokemonList[i].height +' is a large pokemon</p>')
       }
     }
     
-    pokemonList.push(pokemonList);
-    console.log(pokemonList);
     
