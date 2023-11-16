c11727c7414088dbe9751b6afebe42e412e223fb
stefxmas <143958588+stefxmas@users.noreply.github.com>
2023-11-16 12:41:54 -0500

    Repository&List, old.js
    fixed Repository & List
    getAll and forEach in the same line
    scrip.old

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,10 @@
-// let pokemonList=[
-//     {name:"Charmander", type:"Fire", height:"0.6"},
-//     {name:"Machop", type:"fighting", height:"1.2"},
-//     { name:"Gengar", type:["ghost","poison"], height:"3"}
-//     ];
-    
-//     for (let i=0; i<pokemonList.length; i++){
-//       if (pokemonList[i].height <1 && pokemonList[i].height >0.5){
-//         console.log(pokemonList[i].name + " is a small pokemon");
-//         document.write('<p>' + pokemonList[i].name +' '+ pokemonList[i].height + ' '+ ' is a large pokemon</p>')
-//       }else if (pokemonList[i].height >1 && pokemonList[i].height <1.5){
-//         console.log(pokemonList[i].name + " is a medium pokemon");
-//         document.write('<p>' + pokemonList[i].name +' '+  pokemonList[i].height + ' is a medium pokemon</p>')
-//       }else {
-//         console.log(pokemonList[i].name + " is a large pokemon");
-//         document.write('<p>' + pokemonList[i].name +' '+  pokemonList[i].height +' is a large pokemon</p>')
-//       }
-//     }
-//     pokemonList.forEach(function(ithem) {
-//         console.log(ithem);
-      
-//       });
-    
-
-      let pokemonList=[
-        {name:"Charmander", type:"Fire", height:"0.6"},
-        {name:"Machop", type:"fighting", height:"1.2"},
-        { name:"Gengar", type:["ghost","poison"], height:"3"}
-        ];
-        
-        let pokemonRepository = (function () {
-          let pokemonList = [
+let pokemonRepository = (function () {
+            
+    let pokemonList = [
             {name:"Charmander", type:"Fire", height:"0.6"},
             {name:"Machop", type:"fighting", height:"1.2"},
-            { name:"Gengar", type:["ghost","poison"], height:"3"}];
+            { name:"Gengar", type:["ghost","poison"], height:"3"}
+        ];
         
         
           function getAll() {
@@ -49,11 +21,11 @@
           };
         })();
         
-        //console.log(pokemonRepository.getAll());
+        // add 
         pokemonRepository.add({ name: "Pikachu", type: "Electric", height:"0.4" });
-        console.log(pokemonRepository.getAll()); 
         
-        pokemonList.forEach(function(pokemon){
+        // display
+        pokemonRepository.getAll().forEach(function(pokemon){
         console.log(pokemon);
         document.write(pokemon.name + " (height: " + pokemon.height + ") , (type: " + pokemon.type + ") ");
         });
---
+++ script.old.js
@@ -0,0 +1,22 @@
+let pokemonList=[
+    {name:"Charmander", type:"Fire", height:"0.6"},
+    {name:"Machop", type:"fighting", height:"1.2"},
+    { name:"Gengar", type:["ghost","poison"], height:"3"}
+    ];
+    
+    for (let i=0; i<pokemonList.length; i++){
+      if (pokemonList[i].height <1 && pokemonList[i].height >0.5){
+        console.log(pokemonList[i].name + " is a small pokemon");
+        document.write('<p>' + pokemonList[i].name +' '+ pokemonList[i].height + ' '+ ' is a large pokemon</p>')
+      }else if (pokemonList[i].height >1 && pokemonList[i].height <1.5){
+        console.log(pokemonList[i].name + " is a medium pokemon");
+        document.write('<p>' + pokemonList[i].name +' '+  pokemonList[i].height + ' is a medium pokemon</p>')
+      }else {
+        console.log(pokemonList[i].name + " is a large pokemon");
+        document.write('<p>' + pokemonList[i].name +' '+  pokemonList[i].height +' is a large pokemon</p>')
+      }
+    }
+    pokemonList.forEach(function(ithem) {
+        console.log(ithem);
+      
+      });
